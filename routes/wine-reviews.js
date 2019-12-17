becd582e3591a74a45319ebe606f6fe9b2e012d4
Brianna Grizzell <breegrizzell@gmail.com>
2019-12-17 12:52:17 -0800

    first draft

---
+++ routes/wine-reviews.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+const wineReviews = require("../db/wine-reviews");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(wineReviews)
+});
+
+module.exports = router;
