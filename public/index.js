6999fa134a38c3745c59dfceee48e88419406b56
Brianna Grizzell <breegrizzell@gmail.com>
2019-12-18 13:00:43 -0800

    beginning of function setups
    Took 32 minutes

--- public/index.js
+++ public/index.js
@@ -3,23 +3,49 @@ async function postData() {
   // This data will show up in the resolve callback
   return await response.json();
 }
+
 postData().then((reviews) => {
     renderData(reviews)
   }
 ).catch(() => {
 });
+
+const reviewsModel = {
+  props: {
+    headers: {
+      label: undefined,
+      sortState: undefined
+    },
+    reviews: undefined,
+    sortLabel: undefined  //price, reviewer, etc
+  }
+};
+
 function renderData(reviews) {
   renderHeaders(reviews);
   renderRows(reviews);
 }
+
 function renderHeaders(reviews) {
   //grab headers from data
+  
   const headers = Object.keys(reviews[0]).map(key => key.replace(/_/g, ' '));
-  console.log(`headers: ${headers}`)
+  headers.createElement('th', '', headers, headers);
+  
+  
 }
-function renderRows(data) {
+
+function renderRows(reviews) {
   //Grab info from rows
+  const rowData = Object.values(reviews).map(value => value.replace(/_/g, ' '));
+  console.log(`Data: ${rowData}`)
 }
-function createElement(tag, className, innerHTML) {
-const element = document.createElement(tag)
+
+function createElement(tag, className, innerHTML, prop) {
+  const element = document.createElement(tag);
+  element.setAttribute('class', className);
+  element.setAttribute('data-prop', prop);
+  element.innerHTML = innerHTML;
+  return element;
+  
 }
