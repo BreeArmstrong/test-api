f5355643d6bf2691ef151a399275d3e801c82856
Brianna Grizzell <breegrizzell@gmail.com>
2019-12-18 13:36:24 -0800

    Setting up model
    Took 17 minutes

--- public/index.js
+++ public/index.js
@@ -21,17 +21,30 @@ const reviewsModel = {
   }
 };
 
+function showData(reviews) {
+  const headers = renderHeaders(reviews);
+  reviewsModel.headers = headers.reduce((sortState, header) => {
+    sortState[header] = {
+      sortState: undefined,
+      label: header
+    };
+    return sortState;
+  }, {});
+  reviewsModel.reviews = renderRows(reviews)
+}
+
 function renderData(reviews) {
+  console.log(reviewsModel);
+  
   renderHeaders(reviews);
   renderRows(reviews);
 }
 
 function renderHeaders(reviews) {
   //grab headers from data
-  
+  const thead = document.querySelector('thead');
   const headers = Object.keys(reviews[0]).map(key => key.replace(/_/g, ' '));
-  headers.createElement('th', '', headers, headers);
-  
+  createElement('th', '', headers, reviewsModel.headers.label);
   
 }
 
