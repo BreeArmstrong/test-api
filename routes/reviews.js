237b82733b383a45b967f44d0a5de7b32b9d9a7b
BP Mishra <bindhyeswari.mishra@gmail.com>
2020-01-14 16:20:31 -0800

    fixed

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,9 +4,15 @@ const uuid = require('uuid');
 
 
 const wineReviews = require("../db/wine-reviews");
+const resultsPerPage = 20;
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
+  const {page} = req.query;
+  if (page) {
+    const startIndex = (page - 1) * 20;
+    res.json(wineReviews.slice(startIndex, startIndex + 20));
+  }
   res.json(wineReviews)
 });
 
