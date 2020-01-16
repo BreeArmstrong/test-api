6acd0cc937f4db2751a48752cb250ffb5eae80cc
BP Mishra <bindhyeswari.mishra@gmail.com>
2020-01-15 20:22:04 -0800

    complete webpack

---
+++ public/precache-manifest.3844e8b0aa41379c878ab927f7830ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce20e5b590b31b10e363bb1a83657a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6040beca16753ca4e75",
+    "url": "/static/css/main.0037b964.chunk.css"
+  },
+  {
+    "revision": "6fa9a54f11f02612ab8b",
+    "url": "/static/js/2.1abe4a86.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/static/js/2.1abe4a86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6040beca16753ca4e75",
+    "url": "/static/js/main.a96dae65.chunk.js"
+  },
+  {
+    "revision": "169b59480d85a34a1789",
+    "url": "/static/js/runtime-main.d077dedd.js"
+  }
+]);
